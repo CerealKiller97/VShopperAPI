b536d19b1bbe6cedf76c6ae333836564c33ecb3e
Stefan <bogdanovicstefan1997@gmail.com>
2019-03-04 10:26:36 +0100

    fixed product migration

--- database/migrations/2016_03_03_150815_create_products_table.php
+++ database/migrations/2016_03_03_150815_create_products_table.php
@@ -18,8 +18,27 @@ class CreateProductsTable extends Migration
             $table->unsignedInteger('account_id');
             $table->unsignedInteger('unit_id');
             $table->unsignedInteger('brand_id');
+            $table->unsignedInteger('vendor_id');
+
             $table->string('name');
             $table->text('description');
+
+            $table->foreign('account_id')
+                  ->references('id')
+                  ->on('accounts');
+
+            $table->foreign('unit_id')
+                  ->references('id')
+                  ->on('units');
+
+            $table->foreign('brand_id')
+                  ->references('id')
+                  ->on('brand');
+
+            $table->foreign('vendor_id')
+                  ->references('id')
+                  ->on('vendors');
+
             $table->timestamps();
         });
     }
